722014657df6d1512c27463e94b411e8025b8c66
David Perez Moreno <davidperezmoreno14@gmail.com>
2024-09-30 12:13:52 +0200

    Conexion React con el back-end

--- gestor-tareas-api/gestor-tareas-frontend/src/pages/Home.js
+++ gestor-tareas-api/gestor-tareas-frontend/src/pages/Home.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Home() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // Hacer la solicitud GET al backend para obtener las tareas
+    axios.get('http://localhost:5000/api/tasks')
+      .then(response => {
+        setTasks(response.data);  // Guardamos las tareas en el estado
+      })
+      .catch(error => {
+        console.error('Error al obtener las tareas:', error);
+      });
+  }, []);  // El array vacío [] significa que el efecto solo se ejecutará una vez al cargar el componente
+
   return (
     <div>
-      <h1>Página Principal</h1>
-      <p>Bienvenido a la página principal del Gestor de Tareas.</p>
+      <h1>Tareas</h1>
+      {tasks.length === 0 ? (
+        <p>No hay tareas disponibles.</p>
+      ) : (
+        <ul>
+          {tasks.map(task => (
+            <li key={task._id}>{task.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
