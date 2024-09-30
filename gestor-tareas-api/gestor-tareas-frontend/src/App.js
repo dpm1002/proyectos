6d3e842722153a8f394f072ba360f9b224248549
David Perez Moreno <davidperezmoreno14@gmail.com>
2024-09-30 11:40:17 +0200

    FrontEnd con React
    Uso la libreria de React para crear el FrontEnd

---
+++ gestor-tareas-api/gestor-tareas-frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* Menú de navegación */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/about">Acerca de</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Definición de rutas */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ gestor-tareas-api/gestor-tareas-frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Acerca de</h1>
+      <p>Esta es la página "Acerca de" de nuestra aplicación Gestor de Tareas.</p>
+    </div>
+  );
+}
+
+export default About;
