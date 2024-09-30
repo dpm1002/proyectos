171df9d537f7041d0f84690fdd1639524ca3649c
David Perez Moreno <davidperezmoreno14@gmail.com>
2024-09-30 10:37:46 +0200

    Creacción inicial de API RESTful para un gestor de tareas

---
+++ gestor-tareas-api/src/app.js
@@ -0,0 +1,18 @@
+// Importamos las librerías necesarias para el servidor
+const express = require('express');  // Framework que nos permite crear el servidor y definir rutas
+const cors = require('cors');  // Middleware que nos permite evitar problemas de seguridad al consumir la API desde otro dominio
+
+// Creamos una aplicación de Express
+const app = express();
+
+// Middleware
+app.use(cors());  // Habilitamos CORS para permitir solicitudes desde otros dominios
+app.use(express.json());  // Middleware para que Express entienda JSON en las solicitudes que recibe
+
+// Definimos un puerto para nuestro servidor
+const PORT = process.env.PORT || 5000;
+
+// Arrancamos el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
