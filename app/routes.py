9d152e1c5b7476fe4995ce35eaf7c764db06820e
David Perez Moreno <davidperezmoreno14@gmail.com>
2025-01-23 10:15:11 +0100

    Arreglos en la generacion de keys

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,7 @@ bp = Blueprint('routes', __name__)
 # Cargar las variables del archivo .env
 load_dotenv()
 
+# Los datos no son expuestos
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
